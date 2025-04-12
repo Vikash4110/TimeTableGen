ab86662323b2a33b5125fa2362fe8a4c2436606a
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2025-04-12 23:53:37 +0530

    main

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://eduationforall.vercel.app'],
+  origin: ['http://localhost:5173','https://eduationforall.vercel.app','https://time-table-gen.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
